4c877822c2822a1db2651af107690beab18c96af
nabeelzanabeet <nabeelzanabeet@gmail.com>
2018-01-01 12:16:20 +0200

    1.1

--- resources/views/Pages/index.blade.php
+++ resources/views/Pages/index.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
        <h1 align="center" ><b>Present Like A Pro</b> </h11>
-       <h2> This is the home page </h2>
+       <h2> This is the hommmmme page </h2>
        <h3>{{$title}}</h3>
 @endsection
